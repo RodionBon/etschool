75bf18bd6ab87604e14d5ba6d3ca1d98ee47876e
Rodion <rodionb4649@gmail.com>
2024-09-22 13:06:12 +0300

    Update events-board.tsx

--- client/src/pages/events-board.tsx
+++ client/src/pages/events-board.tsx
@@ -2,27 +2,16 @@ import CardList from "../components/card-list";
 import CardsPagination from "../components/cards-pagination";
 import IEvent from "../interfaces/event";
 import { useEffect, useState } from "react";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select";
 import { useParams } from "react-router-dom";
 
 const EventsBoard = () => {
 	const [events, setEvents] = useState<IEvent[]>([]);
-	const [sortBy, setSortBy] = useState<string>("id");
 	const { page } = useParams();
 	const pageNumber = Number(page);
 	const [pagesCount, setPagesCount] = useState<number>(0);
 
 	useEffect(() => {
-		const params = new URLSearchParams({
-			sortBy,
-		});
-		fetch(`http://localhost:3001/events/${page}?${params}`)
+		fetch(`http://localhost:3001/events/${page}`)
 			.then((response) => response.json())
 			.then((data) => setEvents(data))
 			.catch((error) => console.error("Error fetching data:", error));
@@ -31,7 +20,7 @@ const EventsBoard = () => {
 			.then((response) => response.json())
 			.then((data) => setPagesCount(data[0]["COUNT"]))
 			.catch((error) => console.error("Error fetching data:", error));
-	}, [sortBy]);
+	}, []);
 
 	return (
 		<>
